008e596a028ef34757e1d33d143555ab70708820
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-12 18:58:55 -0500

    Rutas básicas

---
+++ resources/views/mostrar-fecha.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mostrar Fecha</title>
+</head>
+
+<body>
+    <h1><?=$titulo?></h1>
+    <?=$fecha?>
+    <br>
+    <a href="/">Inicio</a>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+use PhpParser\Node\Expr\PostDec;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    // echo "<h1>Hola mundo de Marco</h1>";
+});
+
+// GET   : Conseguir datos
+// POST  : Guardar datos
+// PUT   : Actualizar recursos
+// DELETE: Eliminar recursos
+
+Route::get('/mostrar-fecha', function () {
+    $titulo = "Estoy mostrando la fecha";
+    $fecha = date('d-m-Y');
+    return view('mostrar-fecha', array(
+        'titulo' => $titulo,
+        'fecha' => $fecha
+    ));
+});
