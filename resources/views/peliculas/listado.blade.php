9a343f4bbcc6c82c411e1b452036f1c0c294acd4
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-12 23:17:22 -0500

    Bucles, condicionales en blade

--- resources/views/peliculas/listado.blade.php
+++ resources/views/peliculas/listado.blade.php
@@ -1,3 +1,40 @@
 <h1>{{$titulo}}</h1>
 <h2>{{$listado[1]}}</h2>
 <p>{{date('d-m-Y')}}</p>
+
+{{-- Comentarios con Blade --}}
+
+{{-- Mostrar cuando existe en php --}}
+<?=isset($director) ? $director : 'No hay director'; ?>
+<br>
+{{-- Mostrar cuando existe en Blade --}}
+{{$dir ?? 'No existe $dir'}}
+
+<br>
+{{-- Condicionales en blade --}}
+@if ($titulo && count($listado) >= 4)
+    <h1>El titulo existe y es este: {{$titulo}}, y el listado es mayour a 3</h1>
+@elseif($titulo)
+    <h1>El titulo existe pero el listado no es mayor a 3</h1>
+@else 
+    <h1>El titulo no existe</h1>
+@endif
+
+{{-- Bucles --}}
+
+@for ($i = 1; $i <= 20; $i++)
+    El numero es: {{$i}} <br>
+@endfor
+
+<hr>
+<?php $contador = 1 ?>
+@while ($contador <= 50)
+    @if ($contador % 2 == 0)
+        NUMERO PAR: {{$contador}} <br>
+    @endif
+    <?php $contador++; ?>
+@endwhile
+
+@foreach ($listado as $pelicula)
+    <p>{{$pelicula}}</p>
+@endforeach
